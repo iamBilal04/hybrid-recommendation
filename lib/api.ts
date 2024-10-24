78005ffa869ad4e6b32cd85c890de5aa786c509b
Bilal Ansari <imbilal164@gmail.com>
2024-10-24 22:26:08 +0530

    Dine

--- lib/api.ts
+++ lib/api.ts
@@ -1,7 +1,7 @@
 // lib/api.ts
 import axios from "axios";
 
-const API_URL = "https://f372-43-231-238-206.ngrok-free.app/";
+const API_URL = process.env.NEXT_PUBLIC_API_URL || "https://f372-43-231-238-206.ngrok-free.app";
 
 const api = axios.create({
   baseURL: API_URL,
